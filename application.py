518b86887c4dde7680b68a9a44171ee6d6a14cc7
desmondcsc7126 <101800952+desmondcsc7126@users.noreply.github.com>
2022-07-22 04:46:08 +0800

    Update application.py

--- application.py
+++ application.py
@@ -3,5 +3,5 @@ from website import createApp
 application = createApp()
 
 if __name__ == '__main__':
-    application.run(debug=True)
-    # application.run(host='0.0.0.0', port=80, debug=True)
+    # application.run(debug=True)
+    application.run(host='0.0.0.0', port=80, debug=True)
